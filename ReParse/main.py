e94749b8eff3b5473eaf7e99228aa728f3468d62
mas6y6 <mark.lopez.jr2011@icloud.com>
2024-08-29 20:07:48 -0500

    Co-authored-by: malachi196 <malachi196@users.noreply.github.com>

--- ReParse/main.py
+++ ReParse/main.py
@@ -1,1 +1,25 @@
-import os
+import os, math, subprocess, io
+import tkinter.messagebox
+import tkinter
+import ttkbootstrap as ttk
+from pathlib import Path
+import time
+
+VERBOSE = True
+
+os.environ["__COMPAT_LAYER"] = "RunAsInvoker"
+compatlayer = os.getenv("__COMPAT_LAYER")
+
+if not tkinter.messagebox.askokcancel("Beta","This project is in devlopment not everything will work.\nDo you want to continue?"):
+    exit()
+
+print("Starting...")
+app = tkinter.Tk()
+app.title("ReParse Development")
+app.geometry("600x500")
+mainframe = ttk.Frame()
+mainframe.pack(fill='both')
+if VERBOSE:
+    print(f"ttkbootstrap import SUCCESSFUL - {time.time()}")
+    tkinter.messagebox.showinfo("Development Warning","You are running this program in development mode which means that development features")
+app.mainloop()
