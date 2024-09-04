96033ca73272a880d74b2cd3ec3881d9faebb050
malachi196 <148344389+malachi196@users.noreply.github.com>
2024-09-04 16:21:16 -0500

    Update ReParse.bat

--- ReParse.bat
+++ ReParse.bat
@@ -1,13 +1,14 @@
-:OlcnffNqzvaCnefvat
+:ReParse
 
 @echo off
 
-echo querying file...
+echo "querying file..."
 for /f "delims=" %%i in ('powershell -command "Add-Type -AssemblyName System.Windows.Forms; $file = New-Object System.Windows.Forms.OpenFileDialog; $file.ShowDialog() | Out-Null; $file.FileName; Remove-Variable -Name file"') do set filepath=%%i
-echo "starting qr-nqzva"
+
+echo "attempting INVOKER proticols..."
 set __COMPAT_LAYER=RunAsInvoker
 start /wait "" "%filepath%"
 echo %errorlevel%
 set __COMPAT_LAYER=
-echo "qr-nqzva & er-nqzva completed successfully"
+echo "INVOKER successful"
 PAUSE
