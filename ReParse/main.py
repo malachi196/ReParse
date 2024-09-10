22e7b3ac28e8e1086dccb5a81e9bf8d0c220449d
malachi196 <148344389+malachi196@users.noreply.github.com>
2024-09-10 12:34:47 +0000

    updated py

--- ReParse/main.py
+++ ReParse/main.py
@@ -7,10 +7,7 @@ import time
 
 VERBOSE = False
 
-os.environ["__COMPAT_LAYER"] = "RunAsInvoker"
-compatlayer = os.getenv("__COMPAT_LAYER")
-
-if not tkinter.messagebox.askokcancel("Beta","This project is in devlopment not everything will work.\nDo you want to continue?"):
+if not tkinter.messagebox.askokcancel("Beta Version","This project is in devlopment not everything will work.\nDo you want to continue?"):
     exit()
 
 print("Starting...")
@@ -19,9 +16,14 @@ if VERBOSE:
     print(f"ttkbootstrap import SUCCESSFUL - {time.time()}")
     tkinter.messagebox.showinfo("Development Warning","You are running this program in development mode which means that development features")
 
-#app = tkinter.Tk()
-#app.title("ReParse Development")
-#app.geometry("600x500")
-#mainframe = ttk.Frame()
-#mainframe.pack(fill='both')
-#app.mainloop()
+def _main(file):
+    os.environ["__COMPAT_LAYER"] = "RunAsInvoker"
+    subprocess.Popen(f"start {file}", shell=False)
+    os.environ["__COMPAT_LAYER"] = ""
+
+app = tkinter.Tk()
+app.title("ReParse Development")
+app.geometry("600x500")
+mainframe = ttk.Frame()
+mainframe.pack(fill='both')
+app.mainloop()
