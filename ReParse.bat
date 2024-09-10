2a1c356a59bb1dad749deaaf13edfc46ed552063
malachi196 <148344389+malachi196@users.noreply.github.com>
2024-09-10 12:33:29 +0000

    Updated ReParse.bat 1.1.0

--- ReParse.bat
+++ ReParse.bat
@@ -1,14 +1,74 @@
-:ReParse
+::-----ReParse-----::
 
 @echo off
+echo starting REPARSE
+call:rparselogo
 
-echo "querying file..."
+echo querying file...
 for /f "delims=" %%i in ('powershell -command "Add-Type -AssemblyName System.Windows.Forms; $file = New-Object System.Windows.Forms.OpenFileDialog; $file.ShowDialog() | Out-Null; $file.FileName; Remove-Variable -Name file"') do set filepath=%%i
+echo file chosen: %filepath%
+set /p fpath="is this correct? [y,n]> "
+IF "%fpath%"=="y" (
+   call:invokeaccess "%filepath%"
+) ELSE (
+   echo "exited REPARSE"
+   echo:
+   PAUSE
+   exit
+)
 
-echo "attempting INVOKER proticols..."
+::funcs::
+:invokeaccess
+echo attempting REPARSE proticols...
 set __COMPAT_LAYER=RunAsInvoker
-start /wait "" "%filepath%"
-echo %errorlevel%
+start /wait "" "%~1"
 set __COMPAT_LAYER=
-echo "INVOKER successful"
+IF "%errorlevel%"=="0" (
+   echo REPARSE successful
+) ELSE IF "%errorlevel%"=="1602" ( ::userinstallcancellation
+   echo REPARSE successful
+) ELSE (
+   echo:
+   echo exit code "%errorlevel%"
+   echo REPARSE warning: multiple things may have happened;
+   echo this might include:
+   echo    - "%~1" may require admin privileges
+   echo    - program was cancelled by the user
+   echo    - something entirely different...
+   echo Check the ReParse github documentation for more information
+   echo https://github.com/malachi196/ReParse/blob/main/errors-and-codes.md
+)
+echo:
 PAUSE
+exit
+goto:EOF
+
+:rparselogo
+(
+echo:
+echo:
+echo               ###  ##############                       
+echo               #######################                   
+echo               ########        #########                 
+echo                                     #####             
+echo         **                              **              
+echo        ####         ##########                          
+echo       #####         **********###                       
+echo       ####          ***#   ***###          ##           
+echo       ####          ***#   ***###         ######        
+echo       ###           *********####        #########      
+echo       ####          ***#########        #######         
+echo       ####          ***########           ####          
+echo       ####          ***#   #####          ####          
+echo        ####         ####     ####        ####           
+echo        #####       ######    #####      #####           
+echo         #####       #####      ####    #####            
+echo           #####    *******      #### #####              
+echo             #######              #######                
+echo               ########################                  
+echo                  ##################                     
+echo                        #####                            
+echo:
+echo:
+)
+goto:EOF
